913861bb9f015150b70525b7b5042750c6c79f66
DiyaRamani <148998068+DiyaRamani@users.noreply.github.com>
2023-12-22 13:18:22 +0530

    Update firebase.config.js

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -6,7 +6,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-/* const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAEcvgFj0yavFL0pYAcAR4bv3DTalSv2x8",
   authDomain: "clone-31145.firebaseapp.com",
   projectId: "clone-31145",
@@ -14,8 +14,8 @@ import { getAuth } from "firebase/auth";
   messagingSenderId: "323523310555",
   appId: "1:323523310555:web:b7ea4e642dc12fadb3edfe",
   measurementId: "G-C9Z60MP09V"
-}; */
-const firebaseConfig = {
+}; 
+/* const firebaseConfig = {
   apiKey: "AIzaSyAyspiS7_3zZ-72MkO2UhV_RPnaDSjxpZs",
   authDomain: "fir-f37c9.firebaseapp.com",
   projectId: "fir-f37c9",
@@ -23,7 +23,7 @@ const firebaseConfig = {
   messagingSenderId: "826140219357",
   appId: "1:826140219357:web:137bdf2e00460d4f52c834",
   measurementId: "G-77V3JJE07R"
-};
+}; */
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
